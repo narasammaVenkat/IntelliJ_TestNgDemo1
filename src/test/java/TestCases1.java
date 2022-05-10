f94c5981e21806b9ae2edc16bdda31eb33dd51f9
NarasammaVenkat <narasammageeta@gmail.com>
2022-05-10 12:32:55 +0530

    my first commit

---
+++ src/test/java/TestCases1.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class TestCases1 {
+    @Test
+    void test1(){
+        System.out.println("I am inside test1");
+    }
+}
